06c6ea801ccc8cd14a1a3227779f1fd67394be5b
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-06-17 16:56:45 -0700

    Finished 'Meet another React component'.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
+
 const list = [
     {
         title: 'React',
         url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
+        author: ' Jordan Walke ',
         num_comments: 3,
         points: 4,
         objectID: 0,
@@ -12,7 +13,7 @@ const list = [
     {
         title: 'Redux',
         url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
+        author: ' Dan Abramov, Andrew Clark ',
         num_comments: 2,
         points: 5,
         objectID: 1,
@@ -22,28 +23,51 @@ const list = [
 function App() {
     return (
         <div>
-            <h1>My Hacker Stories</h1>
+            <h1>My Hacker Stories</h1>  {/* Header */}
 
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
+            <Search/>
 
-            <hr />
+            <hr/>
 
-            <ul>
-                {list.map(function (item) {
-                    return (
-                        <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-                            <span>{item.author}</span>
-                            <span>{item.num_comments}</span>
-                            <span>{item.points}</span>
-                        </li>
-                    );
-                })}
-            </ul>
+            <List/>
         </div>
+    );
+}
+
+/**
+ * Component function which holds the logic for the search bar
+ * @returns {JSX.Element} search bar label and input box
+ */
+function Search() {
+
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text"/>
+        </div>
+    )
+}
+
+/**
+ * Component function which contains the logic for the list
+ * @returns {JSX.Element}
+ */
+function List() {
+    return (
+        <ul>
+            {list.map(function (item) {
+                return (
+                    <li key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </li>
+                );
+            })}
+        </ul>
     );
 }
 
